20be6728f4d5636258744691af92317cf571bf03
nachoperna <ignacioperna@gmail.com>
2024-10-07 23:47:49 -0300

    correcciones

--- TPE/Consigna 2.sql
+++ TPE/Consigna 2.sql
@@ -3,31 +3,44 @@
    generar e insertar los registros asociados a la/s factura/s correspondiente/s a los distintos
    clientes. Indicar si se deben proveer parámetros adicionales para su generación y, de ser así, cuales. */
 
+   create or replace procedure generarFacturas()
+       language 'plpgsql' as $$
+       begin
+           insert into lineacomprobante
+           values (
+                   
+                  )
+       end;
+       $$
+
+   -- tambien de forma recursiva?
    
 -- b.
 /* Al ser invocado entre dos fechas cualesquiera genere un informe de los empleados
 (personal) junto con la cantidad de clientes distintos que cada uno ha atendido en tal periodo y
 los tiempos promedio y máximo del conjunto de turnos atendidos en el periodo.*/
 
-create or replace function generarInformePersonal(timestamp inicio_periodo, timestamp fin_periodo)
-   returns table (
-      id_personal int,
-      nombre_personal varchar(40),
-      cantidad_clientes_atendidos int,
-      desde timestamp set value inicio_periodo,
-      hasta timestamp set value fin_periodo,
-      tiempo_promedio_turno timestamp,
-      tiempo_maximo_turno timestamp
-   )
-   as $$
-   begin
-      return query
-      (select p.id_personal, p1.nombre, count(distinct c.id_cliente), avg(t.desde - t.hasta), max(t.desde - t.hasta)
-       from personal p 
-       join persona p1 on id_personal = id_persona
-       join turno t on t.id_personal = p.id_personal
-       join lugar l on l.id_lugar = t.id_lugar
-       join comprobante c on l.id_lugar = c.id_lugar
+   create or replace function generarInformePersonal(inicio_periodo timestamp, fin_periodo timestamp)
+      returns table (
+         id_personal int,
+         nombre_personal varchar(40),
+         cantidad_clientes_atendidos int,
+         desde timestamp,
+         hasta timestamp,
+         tiempo_promedio_turno timestamp,
+         tiempo_maximo_turno timestamp
       )
-   end;
-   $$ language 'plpgsql';
+      as $$
+      begin
+         return query
+         (select p.id_personal, p1.nombre, count(distinct c.id_cliente), inicio_periodo, fin_periodo, avg(t.desde - t.hasta), max(t.desde - t.hasta)
+          from personal p
+              join persona p1 on p.id_personal = p1.id_persona
+              join turno t on t.id_personal = p.id_personal
+              join lugar l on l.id_lugar = t.id_lugar
+              join comprobante c on l.id_lugar = c.id_lugar
+          where t.desde >= inicio_periodo and t.hasta <= fin_periodo
+          group by t.id_personal
+         );
+      end;
+      $$ language 'plpgsql';
